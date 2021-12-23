acf5f8a813a62e361d41dc09ea3d984dcab42331
messejak22 <70961425+messejak22@users.noreply.github.com>
2021-12-23 15:03:55 -0600

    Version 2.0
    Now can count with JFrame one word at a time

--- Main.java
+++ Main.java
@@ -1,21 +1,56 @@
-import java.util.*;
-public class Main {
-    
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Main extends JFrame {
+
+    JPanel panel = new JPanel();
+    JLabel label = new JLabel();
+    JLabel label2 = new JLabel();
+    JTextField textField = new JTextField(30);
+    JButton button = new JButton("Enter");
+    public static String input;
+    public static LinkedList<String> l1;
+
+    public Main(){
+        setTitle("Tutorial");
+        setVisible(true);
+        setSize(400, 200);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        panel.add(textField);
+
+
+        textField.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                input = textField.getText();
+                label.setText(input); 
+            } // end action performed 
+        }); // end action listener
+
+        panel.add(button);
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String input = textField.getText();
+                l1.addToFront(input);
+                String finalInput = input + " " + String.valueOf(l1.getSize());
+                label.setText(finalInput);
+            } // end actionPerformed
+        }); // end action listener 
+              
+        panel.add(label);
+        add(panel);
+        
+
+    } // end constructor 
+
     public static void main(String[] args) {
-        
-
-        LinkedList<String> l1 = new LinkedList<String>();
-        l1.addToFront("is");
-        l1.addToFront("Jake");
-        l1.addToBack("the");
-        l1.addToBack("best");
-
-        System.out.println(l1);
-        System.out.println(l1.getSize());
-        
-
-
-
-        
+        Main m = new Main();
+        l1 = new LinkedList<String>();
     }
 }
